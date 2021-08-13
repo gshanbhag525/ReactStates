c7a27d1c25a46fc5f212d2a0a0c61f9103e7b8a8
gshanbhag525 <17478096+gshanbhag525@users.noreply.github.com>
2021-08-13 22:00:33 +0530

    asynccall by setState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      meanOfLife : 43
+    }
+  }
+  
+  handleClick = () => {
+    this.setState(
+      {meanOfLife: this.state.meanOfLife +1}
+    )
+    console.log(this.state);
+  }
+
+  render() {  
+    return(
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            {this.state.meanOfLife}
+          </p>
+          <button onClick={this.handleClick}>
+            update STATE
+          </button>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
